084e488703c3db738c6f0e943d0c9326d53cb67f
zhangyuiris <913007768zyw@gmail.com>
2018-10-26 15:21:32 +0800

    init

---
+++ movie-board/src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'movie-board'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
---
+++ movie-board/src/renderer/components/Index.vue
@@ -0,0 +1,41 @@
+<template>
+  <Row class="top">
+    <Col span="20" offset="2">
+
+    </Col>
+  </Row>
+</template>
+
+<script>
+  import movie from '../assets/json'
+  export default {
+    name: 'landing-page',
+    components: {},
+    data () {
+      return {
+        requireThree: [],
+        requireFour: [],
+        requireFive: []
+      }
+    },
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    },
+    created () {
+      console.log(movie)
+      movie.forEach(item => {
+        console.log(item.favor.length)
+        // if (item.favor.length === 3) {
+        //   this.requireThree.append(item)
+        // }
+      })
+      console.log(this.requireThree)
+    }
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+</style>
